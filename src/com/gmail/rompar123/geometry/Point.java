872ed764350a990fbc64a05ff608f74b6a8ef5da
Roman <rompar123@gmail.com>
2017-06-20 11:02:40 +0300

    -

---
+++ src/com/gmail/rompar123/geometry/Point.java
@@ -0,0 +1,14 @@
+package com.gmail.rompar123.geometry;
+
+public class Point {
+
+	private int x,y;
+	
+	public Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX(){ return x; }
+	public int getY(){ return y; }
+}
